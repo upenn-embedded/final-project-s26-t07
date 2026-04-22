a38eee215d3b66a7a37d334667c3e3cbb7c283af
Lori Brown <34607662+Lori2018@users.noreply.github.com>
2026-04-22 18:39:03 -0400

    MOTOR WORKSSS

--- code/motor.c
+++ code/motor.c
@@ -2,30 +2,51 @@
 #include <avr/io.h>			/* Include AVR std. library file */
 #include <util/delay.h>		/* Include delay header file */
 
-#define MOTOR_STEP PD0
-#define MOTOR_DIR PD2
+#define TURNTABLE_MOTOR_STEP   PD5
+#define TURNTABLE_MOTOR_DIR    PD4
+#define TURNTABLE_MOTOR_SLEEP  PD3
+#define PUMP                   PD2
 
 void initialize() {
+
     // set pin direction to output
-    DDRD |= (1 << DDD0);
-    DDRD |= (1 << DDD2);
+    DDRD |= (1 << TURNTABLE_MOTOR_STEP);
+    DDRD |= (1 << TURNTABLE_MOTOR_DIR);
 
     // set motor dir
-    PORTD |= (1 << PD2);
-    
+    PORTD |= (1 << TURNTABLE_MOTOR_DIR);
+
+    // set pump dir
+    DDRD |= (1 << PUMP);
+
 }
 
-void move_motor() {
-    PORTD |= (1 << MOTOR_STEP);
-    _delay_us(20); // short pulse
-    PORTD &= ~(1 << MOTOR_STEP);
-    _delay_ms(5); // faster stepping
+void move_motor(int iters) {
+    for (int i = 0; i < iters; i++) {
+        PORTD |= (1 << TURNTABLE_MOTOR_STEP);
+        _delay_us(20); // short pulse
+        PORTD &= ~(1 << TURNTABLE_MOTOR_STEP);
+        _delay_ms(5); // faster stepping
+    }
+}
 
+void motor_off() {
+    PORTD &= ~(1 << TURNTABLE_MOTOR_STEP);
+}
+
+void pump_on(void) {
+    PORTD |= (1 << PUMP);
+}
+
+void pump_off(void) {
+    PORTD &= ~(1 << PUMP);
 }
 
 int main(void) {
     initialize();
+    //    pump_on();
+    move_motor(2000);
 
     while (1) {
-        move_motor();
     }
+}
