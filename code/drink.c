c3aa98876211682675c67944ff72b2fe80af2d7e
Lori Brown <34607662+Lori2018@users.noreply.github.com>
2026-04-23 02:18:34 -0400

    commit

--- code/drink.c
+++ code/drink.c
@@ -4,64 +4,58 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include "uart.h"
+
+#define WATER_FLOW_SENSOR      PD6
 #define TURNTABLE_MOTOR_STEP   PD5
 #define TURNTABLE_MOTOR_DIR    PD4
 #define PUMP                   PD3
-//#define WATER_FLOW_SENSOR    PD5
+#define TURNTABLE_MOTOR_SLEEP  PD2
+#define FLOW_RATE               0.66 // mL / # ticks
+volatile uint32_t pulse_count = 0;
 
-//volatile uint32_t pulse_count = 0;
+ISR(PCINT2_vect) {
+    static uint8_t last_state = 0;
+    uint8_t current_state = (PIND & (1 << WATER_FLOW_SENSOR)) ? 1 : 0;
 
-//
-//ISR(PCINT2_vect) {
-//    static uint8_t last_state = 0;
-//    uint8_t current_state = (PIND & (1 << WATER_FLOW_SENSOR)) ? 1 : 0;
-//
-//    if (current_state && !last_state) {
-//        pulse_count++;
-//    }
-//
-//    last_state = current_state;
-//}
+    if (current_state && !last_state) {
+        pulse_count++;
+    }
 
+    last_state = current_state;
+}
 
 void initialize() {
-
     // set pin direction to output
     DDRD |= (1 << TURNTABLE_MOTOR_STEP);
     DDRD |= (1 << TURNTABLE_MOTOR_DIR);
-    DDRD |= (1 << PD2);
+    DDRD |= (1 << TURNTABLE_MOTOR_SLEEP);
+    DDRD |= (1 << PUMP);
+    DDRD &= ~(1 << WATER_FLOW_SENSOR);
 
     // set motor dir
     PORTD |= (1 << TURNTABLE_MOTOR_DIR);
 
     // set pump dir
-    DDRD |= (1 << PUMP);
-    
+
     // enable pull-up on PD1
-//    PORTD |= (1 << WATER_FLOW_SENSOR);
+    PORTD |= (1 << WATER_FLOW_SENSOR);
 
-//    PCICR |= (1 << PCIE2);
-//    PCMSK2 |= (1 << PCINT17);
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT22);
 
-//    sei();
+    sei();
 
 }
 
 void run_motor(int iters) {
-    PORTD |= (1 << PD2);
+    PORTD |= (1 << TURNTABLE_MOTOR_SLEEP);
     for (int i = 0; i < iters; i++) {
         PORTD |= (1 << TURNTABLE_MOTOR_STEP);
         _delay_us(20); // short pulse
         PORTD &= ~(1 << TURNTABLE_MOTOR_STEP);
         _delay_ms(5); // faster stepping
     }
-    PORTD &= ~(1 << PD2);
-}
-
-void run_pump(int time) {
-    PORTD |= (1 << PUMP);
-    _delay_ms(time);
-    PORTD &= ~(1 << PUMP);
+    PORTD &= ~(1 << TURNTABLE_MOTOR_SLEEP);
 }
 
 void pump_on(void) {
@@ -72,13 +66,29 @@ void pump_off(void) {
     PORTD &= ~(1 << PUMP);
 }
 
-// goal: dispense x amt of water
+// run pump for mL amt of mL
+void run_pump(double mL) {
+    uint32_t count;
+    pump_on();
+    printf("PUMP ON\n");
+
+    cli();
+    count = pulse_count;
+    sei();
+
+    while (count < mL * FLOW_RATE) {
+        cli();
+        count = pulse_count;
+        sei();
+        printf("Pulse count: %lu\r\n", (unsigned long) count);
+    }
+    pump_off();
+    printf("PUMP OFF\n");
+}
+
 int main(void) {
     initialize();
+    uart_init();
     run_motor(2000);
-    _delay_ms(500);
-    run_pump(5000);
-//    pump_on();
-    while (1) {
-    }
+    run_pump(FLOW_RATE * 600);
 }
