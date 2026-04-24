5cae9987d6b2499cacfc6cf0febaff7f8f18b27c
aluo13 <85607684+aluo13@users.noreply.github.com>
2026-04-24 00:09:24 -0400

    integrated with initial ui

--- code/touchscreen_main.c
+++ code/touchscreen_main.c
@@ -1,8 +1,31 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
 #include "tft.h"
 #include "touchscreen.h"
+#include "ui.h"
 
 #define RED 0xF800
 #define BLACK 0x0000
+
+
+void clear_screen() {
+    tft_fill_screen(BLACK);
+}
+
+void test_touchscreen_red_dots() {
+    while(1)
+    {
+        TouchPoint p = touch_get_point();
+
+        if (p.touched) {
+    tft_fill_rect(p.x, p.y, 4, 4, RED);
+    _delay_ms(100);
+    }       
+    }
+    
+}
 
 int main(void)
 {
@@ -10,21 +33,14 @@ int main(void)
     tft_init();
     adc_init();
 
-    tft_fill_screen(BLACK);
+    clear_screen();
+    
+    ui_init();
 
-    while(1)
-    {
-        TouchPoint p = touch_get_point();
-
-        if(p.touched)
-        {
-            tft_fill_rect(
-                p.x,
-                p.y,
-                4,
-                4,
-                RED
-            );
-        }
+    while (1) {
+        ui_update();
+        _delay_ms(100);
     }
+
+
 }
