3d8a51ea26cea5ed57c7a4453a7e4a5a407dd983
aluo13 <85607684+aluo13@users.noreply.github.com>
2026-04-30 00:00:42 -0400

    Add function to set drink names in UI

--- code/ui.h
+++ code/ui.h
@@ -10,6 +10,9 @@ void ui_update(void);
 void ui_draw_home(void);
 void ui_draw_confirm(void);
 void ui_draw_dispense(void);
+void ui_set_drink_names(const char *name_1,
+    const char *name_2,
+    const char *name_3);
 
 uint8_t ui_button_pressed(uint16_t x, uint16_t y, uint16_t w, uint16_t h, TouchPoint p);
 
