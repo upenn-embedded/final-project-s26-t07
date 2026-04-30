d175ead47aaafabda41ab063e2cf74cec8501fcd
aluo13 <85607684+aluo13@users.noreply.github.com>
2026-04-29 23:59:23 -0400

    Add motor control and pump definitions

--- code/touchscreen_main.c
+++ code/touchscreen_main.c
@@ -6,6 +6,7 @@
 #include "touchscreen.h"
 #include "ui.h"
 #include "recipe.h"
+//#include "uart.h"
 
 
 #define BLACK   0x0000
@@ -18,14 +19,38 @@
 #define GRAY    0x8410
 #define DARK    0x4208
 
+#define PUMP2 PD7
+#define PUMP3 PC4
+
+#define TURNTABLE_MOTOR_STEP PD5
+#define TURNTABLE_MOTOR_DIR PD4
+#define TURNTABLE_MOTOR_SLEEP PD2
+
+#define WATER_FLOW_SENSOR PD6
+
 Recipe menu_drinks[3];
 
+void run_motor(int iters) {
+    PORTD |= (1 << TURNTABLE_MOTOR_SLEEP);
+    for (int i = 0; i < iters; i++) {
+        PORTD |= (1 << TURNTABLE_MOTOR_STEP);
+        _delay_us(5);
+        PORTD &= ~(1 << TURNTABLE_MOTOR_STEP);
+        _delay_ms(10);
+    }
+    PORTD &= ~(1 << TURNTABLE_MOTOR_SLEEP);
+}
 
 int main(void)
 {
     spi_init();
     tft_init();
     adc_init();
+    DDRD |= (1 << PUMP2);
+    DDRC |= (1 << PUMP3);
+    
+    PORTD |= (1 << PUMP3);
+    PORTC |= (1 << PUMP2);
 
     menu_drinks[0] = recipe_1;
     menu_drinks[1] = recipe_2;
@@ -45,7 +70,6 @@ int main(void)
     }
 
 }
-
 
 
 
